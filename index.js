f3df6a74970d409facaec33534fd62a29fe753cf
Johnn Castro <jcastro@innovajsl.com>
2018-05-21 14:25:26 -0600

    heroku's needed options added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+console.log('initializing...')
 const express = require('express');
 const app = express();
 app.get('/express', (req, res) => {
     res.send({ hi: 'there' });
 })
-app.listen(5000);
+const PORT = process.env.PORT || 5000; //ENVIRONMENT VARIABLE FOR PORT
+app.listen(PORT);
+console.log(`App running on :${PORT}`);
