ddf4aab2db73e9e8a9e61d6b14e3ca9717df1ffa
Johnn Castro <jcastro@innovajsl.com>
2018-05-21 15:07:47 -0600

    changing get to index /

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 console.log('initializing...')
 const express = require('express');
 const app = express();
-app.get('/express', (req, res) => {
-    res.send({ hi: 'there' });
+app.get('/', (req, res) => {
+    res.send({ hi: 'there index' });
 })
 const PORT = process.env.PORT || 5000; //ENVIRONMENT VARIABLE FOR PORT
 app.listen(PORT);
